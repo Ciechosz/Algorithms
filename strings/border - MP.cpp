fcd91f56043edfac0cad6003668368df074d93bc
Wojciech <ciechosz@gmail.com>
2015-07-17 12:50:56 +0200

    Verbose implementations of MP and KMP string search algorithms.

--- strings/border - MP.cpp
+++ strings/border - MP.cpp
@@ -4,30 +4,37 @@
 #include <iostream>
 
 using namespace std;
+
 // function computes maximum length of string's border
+// for m = s.length()
+// O = 
 int maxBorder(string const& s);
 
 int main() {
+	// test maxBorder()
 	string s;
 	cout << "String: " << endl;
-	//getline(cin, s);
-	s = "ABACABAB";
+	getline(cin, s);
 	cout << maxBorder(s) << endl;
 }
+
 
 int maxBorder(string const& s){
 	int* MPNext = new int[s.length() + 1];
 	MPNext[0] = -1;
 	int b = -1; 
 	for(int i = 1; i <= s.length();++i){
+		
 		while(b > -1 && s[b] != s[i - 1]){
-			//cout << "b == " << b << "; MPNext["<<b<<"] == " << MPNext[b] << endl;
 			b = MPNext[b];
 		}
-		//cout << "End of while. b == " << b << ", i == " << i << ", s[b] == " << s[b] << ", s[i-1] == " << s[i-1] << endl;
 		
+		/* 
+		if b == -1 line below just sets value to 0, meaning prefix s[0:b] has no border
+		if b >0 0 it means, that prefix s[0:b] has border of length 1
+		for b > 0 border length of prefix s[0:b] is derived from extending border of s[0:b-1] by character s[b - 1]
+		*/
 		MPNext[i] = ++b;
-		//cout << "MPNext[i] = ++b == " << MPNext[i] << endl;
 	}
-	return b;
+	return b; // equivalent to return MPNext[s.length()];
 }
---
+++ strings/search - MP.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+
+using namespace std;
+
+vector<int> generateMPNext(string const& s){
+	vector<int> result(s.length()+1);
+	result[0] = -1;
+	int b = -1;
+	for(int i = 1; i <= s.length(); ++i){
+		while(b > -1 && s[b] != s[i-1]){
+			b = result[b];
+		}
+		result[i] = ++b;
+	}
+	return result;
+}
+
+// returns list of positions of the pattern
+list<int> findPattern(string const& haystack, string const& needle){
+	
+	
+	
+	list<int> result;
+	vector<int> MPNext = generateMPNext(needle);
+	cout << "MPNext: " << endl;
+	for(int i = 0; i <= needle.length(); ++i){
+		cout << i << '\t';
+	}
+	cout << endl;
+	cout <<  '\t';
+	for(char c : needle){
+		cout << c << '\t';
+	}
+	cout << endl;
+	for(int i : MPNext){
+		cout << i << '\t';
+	}
+	cout << endl;
+	
+	int i = 0; // for browsing haystack
+	int prefix_len = 0; // length of matching prefix
+	int pos = 0; // position in haystack of matching prefix
+	int b = 0;
+	while(pos <= haystack.length() - needle.length()){
+		// checking consequent characters
+		
+		i = pos + prefix_len;
+		if(needle[prefix_len] == haystack[i]){
+			++prefix_len; // next character is matching as well				
+		} else { // next character (is not matching)
+			if(MPNext[prefix_len] > 0){ // reduce matching prefix to its border
+				cout << "Reduction to border" << endl;
+				prefix_len = MPNext[prefix_len];
+				pos = i - prefix_len;
+			} else {
+				// move window by one
+				++pos;
+			}
+		}
+		
+		if(prefix_len == needle.length()){
+			result.push_back(pos);
+			++pos;
+			prefix_len = 0;
+		}
+	}		
+	
+	return result;
+}
+
+int main(){
+	string hay, needle;
+	cout << "Text: " << endl;
+	getline(cin, hay);
+	cout << "Pattern: " << endl;
+	getline(cin, needle);
+	cout << "Looking for \"" << needle << "\" in \"" << hay << "\"" << endl;
+	list<int> result = findPattern(hay, needle);
+	cout << "Result: " << endl;
+	for (int pos : result){
+			cout << pos << ", ";
+	}
+	cout << endl;
+}
